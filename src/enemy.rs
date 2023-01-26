eddb2b73e21d228de4c1fdb717c56c024adc17c3
kklibo <kklibo4@gmail.com>
2023-01-26 15:37:07 -0800

    enemies follow player

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+use crate::PlayerLocation;
+
 #[derive(Component)]
 pub struct Enemy {
     pub radius: f32,
@@ -24,36 +26,80 @@ pub fn setup(
             next_waypoint: Some(Vec3::ZERO),
         },
     ));
+
+    // enemy cube
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
+            material: materials.add(Color::rgb(0.8, 0.8, 0.1).into()),
+            transform: Transform::from_xyz(-4., 2., 0.).looking_at(Vec3::new(-5., 5., 0.), Vec3::Z),
+            ..default()
+        },
+        Enemy {
+            radius: 0.1,
+            next_waypoint: Some(Vec3::ZERO),
+        },
+    ));
 }
 
-pub fn enemy_movement_system(mut query: Query<(&mut Transform, &mut Enemy)>, time: Res<Time>) {
+pub fn enemy_movement_system(
+    mut query: Query<(&mut Transform, &mut Enemy), Without<PlayerLocation>>,
+    mut query2: Query<&mut Transform, (With<PlayerLocation>, Without<Enemy>)>,
+    time: Res<Time>,
+) {
     const MPS: f32 = 5.0;
     const DPS: f32 = 180.0;
 
     const WAYPOINT_RADIUS_M: f32 = 1.0;
+    const VISION_RADIUS_M: f32 = 2.0;
 
     for (mut loc, mut enemy) in query.iter_mut() {
-        if let Some(x) = enemy.next_waypoint {
-            let to_waypoint = x - loc.translation;
+        let mut selected_target = None;
 
-            if to_waypoint.length() > WAYPOINT_RADIUS_M {
-                if loc.forward().cross(to_waypoint).dot(Vec3::Z) > 0. {
-                    //turn left
-                    let step = DPS * time.delta_seconds();
-                    loc.rotate_z(step.to_radians());
-                } else {
-                    //turn right
-                    let step = DPS * time.delta_seconds() * -1.;
-                    loc.rotate_z(step.to_radians());
+        if selected_target.is_none() {
+            //try to target player
+            if let Some(player_loc) = query2.iter_mut().next() {
+                let to_player = player_loc.translation - loc.translation;
+                if to_player.length() < VISION_RADIUS_M {
+                    selected_target = Some(player_loc.translation);
                 }
-
-                //move forward
-                let step = loc.forward() * MPS * time.delta_seconds();
-                loc.translation += step;
-            } else {
-                //new waypoint
-                enemy.next_waypoint = Some(Vec3::new(-4., -4., 0.));
             }
+        }
+
+        if selected_target.is_none() {
+            //waypoint target
+            if let Some(x) = enemy.next_waypoint {
+                let to_waypoint = x - loc.translation;
+                if to_waypoint.length() < WAYPOINT_RADIUS_M {
+                    //new waypoint
+                    // temp
+                    if enemy.next_waypoint == Some(Vec3::new(-4., -4., 0.)) {
+                        enemy.next_waypoint = None;
+                    } else {
+                        enemy.next_waypoint = Some(Vec3::new(-4., -4., 0.));
+                    }
+                    continue;
+                }
+                selected_target = Some(x);
+            }
+        }
+
+        if let Some(x) = selected_target {
+            let to_target = x - loc.translation;
+
+            if loc.forward().cross(to_target).dot(Vec3::Z) > 0. {
+                //turn left
+                let step = DPS * time.delta_seconds();
+                loc.rotate_z(step.to_radians());
+            } else {
+                //turn right
+                let step = DPS * time.delta_seconds() * -1.;
+                loc.rotate_z(step.to_radians());
+            }
+
+            //move forward
+            let step = loc.forward() * MPS * time.delta_seconds();
+            loc.translation += step;
         }
     }
 }
